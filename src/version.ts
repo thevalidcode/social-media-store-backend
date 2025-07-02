d2ca92834263c8e88907d239eb08539eb5289b4b
Valid Code <thevalidcode@gmail.com>
2025-07-03 00:30:28 +0100

    fix: Updated the script of runing release so it will update the package.json too and fixed the version to load the correct version.

--- src/version.ts
+++ src/version.ts
@@ -1,8 +1,11 @@
-import { readFileSync } from "fs";
+import { readFileSync, existsSync } from "fs";
 import { join } from "path";
 
-const pkg = JSON.parse(
-  readFileSync(join(__dirname, "../package.json"), "utf-8")
-);
+const distPath = join(__dirname, "../package.json");
+const rootPath = join(__dirname, "../../package.json");
+
+const pkgPath = existsSync(distPath) ? distPath : rootPath;
+
+const pkg = JSON.parse(readFileSync(pkgPath, "utf-8"));
 
 export const API_VERSION = pkg.version as string;
