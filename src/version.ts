cbd89ad1af2ac12925508478c0588eec44edf1d5
Valid Code <thevalidcode@gmail.com>
2025-07-03 00:49:24 +0100

    Fixed getting of version for both dev and prod.

--- src/version.ts
+++ src/version.ts
@@ -1,10 +1,10 @@
 import { readFileSync, existsSync } from "fs";
 import { join } from "path";
 
-const distPath = join(__dirname, "../package.json");
-const rootPath = join(__dirname, "../../package.json");
+const distPath = join(__dirname, "./package.json");
+const srcPath = join(__dirname, "../package.json");
 
-const pkgPath = existsSync(distPath) ? distPath : rootPath;
+const pkgPath = existsSync(distPath) ? distPath : srcPath;
 
 const pkg = JSON.parse(readFileSync(pkgPath, "utf-8"));
 
