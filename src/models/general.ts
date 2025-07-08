d2e53e64b7cc90c3ecb000008e2e72313b2c65e1
Valid Code <thevalidcode@gmail.com>
2025-07-08 16:40:40 +0100

    fix: The table creation for the general

--- src/models/general.ts
+++ src/models/general.ts
@@ -2,16 +2,16 @@ import { pool } from "../config/db";
 
 async function createDesignStylesTable() {
   await pool.query(`
-    CREATE TABLE IF NOT EXISTS general (
-      id INTEGER NOT NULL,
-      uid TEXT PRIMARY KEY,
-      store_id INTEGER NOT NULL REFERENCES stores(store_id) ON DELETE CASCADE,
-      title TEXT NOT NULL DEFAULT "Store",
-      logo_url TEXT DEFAULT "https://validpanel.com/assets/ValidPanel-CLfY079M.png",
-      favicon_url TEXT DEFAULT "https://validpanel.com/assets/ValidPanel-CLfY079M.png",
-      default_client_currency TEXT
-    );
-  `);
+  CREATE TABLE IF NOT EXISTS general (
+    id INTEGER NOT NULL,
+    uid TEXT PRIMARY KEY,
+    store_id INTEGER NOT NULL REFERENCES stores(store_id) ON DELETE CASCADE,
+    title TEXT NOT NULL DEFAULT 'Store',
+    logo_url TEXT DEFAULT 'https://validpanel.com/assets/ValidPanel-CLfY079M.png',
+    favicon_url TEXT DEFAULT 'https://validpanel.com/assets/ValidPanel-CLfY079M.png',
+    default_client_currency TEXT
+  );
+`);
 
   const res = await pool.query(`
     SELECT column_name FROM information_schema.columns 
@@ -25,9 +25,9 @@ async function createDesignStylesTable() {
     uid: "ALTER TABLE general ADD COLUMN uid TEXT PRIMARY KEY",
     store_id:
       "ALTER TABLE general ADD COLUMN store_id INTEGER NOT NULL REFERENCES stores(store_id) ON DELETE CASCADE",
-    title: `ALTER TABLE general ADD COLUMN title TEXT NOT NULL DEFAULT "Store"`,
-    logo_url: `ALTER TABLE general ADD COLUMN logo_url TEXT DEFAULT "https://validpanel.com/assets/ValidPanel-CLfY079M.png"`,
-    favicon_url: `ALTER TABLE general ADD COLUMN favicon_url TEXT DEFAULT "https://validpanel.com/assets/ValidPanel-CLfY079M.png"`,
+    title: `ALTER TABLE general ADD COLUMN title TEXT NOT NULL DEFAULT 'Store'`,
+    logo_url: `ALTER TABLE general ADD COLUMN logo_url TEXT DEFAULT 'https://validpanel.com/assets/ValidPanel-CLfY079M.png'`,
+    favicon_url: `ALTER TABLE general ADD COLUMN favicon_url TEXT DEFAULT 'https://validpanel.com/assets/ValidPanel-CLfY079M.png'`,
     default_client_currency:
       "ALTER TABLE general ADD COLUMN default_client_currency TEXT",
   };
