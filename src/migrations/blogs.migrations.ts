538a5d60dd123e8faadf7ad08397701b972c29bd
Ibeh Precious (Valid) <ibeprecious49@gmail.com>
2025-07-24 00:40:00 +0100

    Update blogs.migrations.ts

--- src/migrations/blogs.migrations.ts
+++ src/migrations/blogs.migrations.ts
@@ -5,7 +5,7 @@ async function createBlogsTable() {
     CREATE TABLE IF NOT EXISTS blogs (
       id INTEGER NOT NULL,
       title TEXT NOT NULL,
-      cover_image TEXT NOT NULL,
+      cover_image TEXT NOT NULL DEFAULT '',
       content TEXT NOT NULL,
       description TEXT NOT NULL DEFAULT '',
       status TEXT NOT NULL DEFAULT 'active',
@@ -27,7 +27,7 @@ async function createBlogsTable() {
     id: `ALTER TABLE blogs ADD COLUMN id INTEGER NOT NULL`,
     title: `ALTER TABLE blogs ADD COLUMN title TEXT NOT NULL`,
     content: `ALTER TABLE blogs ADD COLUMN content TEXT NOT NULL`,
-    cover_image: `ALTER TABLE blogs ADD COLUMN cover_image TEXT NOT NULL`,
+    cover_image: `ALTER TABLE blogs ADD COLUMN cover_image TEXT NOT NULL DEFAULT ''`,
     description: `ALTER TABLE blogs ADD COLUMN description TEXT NOT NULL DEFAULT ''`,
     status: `ALTER TABLE blogs ADD COLUMN status TEXT NOT NULL DEFAULT 'active'`,
     position: `ALTER TABLE blogs ADD COLUMN position INTEGER NOT NULL`,
