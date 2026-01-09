3f1fd395289797e8d2cbe0e3391d66c6ced23ac4
Ibeh Precious (Valid) <ibeprecious49@gmail.com>
2026-01-09 16:40:36 +0100

    feat(prisma): integrate PostgreSQL adapter and update database configuration

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,6 +1,6 @@
-import { env } from "./env.config";
 import { PrismaClient } from "../../prisma/generated/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "./env.config";
 
-const prisma = new PrismaClient();
-
-export { prisma };
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+export const prisma = new PrismaClient({ adapter });
